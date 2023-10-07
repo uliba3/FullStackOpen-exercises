245b3339141596a60fbd9e4885f54c214336e27d
uliba3 <60570852+ulilba3@users.noreply.github.com>
2023-10-07 23:56:52 +0900

    complete step3

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,14 +2,16 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-1234567'}
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const addName = (event) => {
     event.preventDefault();
     const nameObject = {
-      name: newName
+      name: newName,
+      number: newNumber
     }
     console.log(nameObject);
     const i = persons.findIndex(person => person.name == nameObject.name);
@@ -26,6 +28,10 @@ const App = () => {
     setNewName(event.target.value)
   }
 
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
@@ -33,13 +39,14 @@ const App = () => {
         <div>
           name: <input value={newName} onChange={handleNameChange}/>
         </div>
+        <div>number: <input value={newNumber} onChange={handleNumberChange}/></div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       {persons.map((person, i) =>
-        <div key={i} >{person.name}</div>
+        <div key={i} >{person.name} {person.number}</div>
       )}
     </div>
   )
