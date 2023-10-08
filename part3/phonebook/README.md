4008a220beec910cfbba4e1769506aa07ff4083b
uliba3 <60570852+ulilba3@users.noreply.github.com>
2023-10-09 03:55:08 +0900

    complete step10

---
+++ part3/phonebook/README.md
@@ -0,0 +1,1 @@
+https://withered-morning-9063.fly.dev/
--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -97,7 +97,7 @@ app.delete('/api/persons/:id', (request, response) => {
     response.status(204).end()
 })
 
-const PORT = 3001
-    app.listen(PORT, () => {
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
