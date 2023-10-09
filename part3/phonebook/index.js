e68eadfcdcc7188cd08578379c462c0e9c4f0890
uliba3 <uliba3@Xubuntu.myguest.virtualbox.org>
2023-10-09 09:35:21 +0900

    complete step1

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const Person = require('./models/person')
 
 app.use(cors())
 app.use(express.json())
@@ -76,7 +77,9 @@ app.post('/api/persons', (request, response) => {
   
   
 app.get('/api/persons', (request, response) => {
-    response.json(persons)
+    Person.find({}).then(persons => {
+        response.json(persons)
+    })
 })
 
 app.get('/api/persons/:id', (request, response) => {
@@ -98,7 +101,7 @@ app.delete('/api/persons/:id', (request, response) => {
     response.status(204).end()
 })
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
