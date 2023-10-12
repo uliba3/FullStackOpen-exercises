70be12132efc98a7a5d2fcabe1bd5ac0fb7d9314
uliba3 <uliba3@Xubuntu.myguest.virtualbox.org>
2023-10-12 09:51:48 +0900

    complete step3

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -77,6 +77,30 @@ test('the first blog is about React patterns', async () => {
     expect(titles).toContain('React patterns')
 })
 
+test('a valid blog can be added', async () => {
+    const newBlog = {
+      title: "async/await simplifies making async calls",
+      author: "async await",
+      url: "https://asyncwaitpatterns.com/",
+      likes: 7
+    }
+  
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+  
+    const response = await api.get('/api/blogs')
+  
+    const titles = response.body.map(r => r.title)
+  
+    expect(response.body).toHaveLength(initialBlogs.length + 1)
+    expect(titles).toContain(
+      'async/await simplifies making async calls'
+    )
+})
+
 afterAll(async () => {
   await mongoose.connection.close()
 })
