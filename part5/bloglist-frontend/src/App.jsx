6c53da7a572bc8e929dcf63c2c43cd9758607c54
uliba3 <uliba3@Xubuntu.myguest.virtualbox.org>
2023-10-25 10:01:10 +0900

    complete 5.2

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -67,6 +67,11 @@ const App = () => {
     }
   }
 
+  const logOut = () => {
+    window.localStorage.clear();
+    setUser(null);
+  }
+
   const loginForm = () => (
     <form onSubmit={handleLogin}>
       <div>
@@ -106,7 +111,7 @@ const App = () => {
       <h2>Blogs</h2>
       {!user && loginForm()} 
       {user && <div>
-        <p>{user.name} logged in</p>
+        <p>{user.name} logged in<button onClick={logOut}>logout</button></p>
         {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
