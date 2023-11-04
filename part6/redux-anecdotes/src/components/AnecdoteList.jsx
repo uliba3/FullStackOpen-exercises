96af309c38600aadfecf9731f093fe97989ad95d
uliba3 <uliba3@Xubuntu.myguest.virtualbox.org>
2023-11-04 13:31:08 +0900

    complete 6.9

--- part6/redux-anecdotes/src/components/AnecdoteList.jsx
+++ part6/redux-anecdotes/src/components/AnecdoteList.jsx
@@ -2,7 +2,20 @@ import { useDispatch, useSelector } from "react-redux"
 import { addVote } from "../reducers/anecdoteReducer"
 
 const AnecdoteList = () => {
-    const anecdotes = useSelector(state => state.slice().sort((a, b) => a.votes - b.votes).reverse())
+    const anecdotes = useSelector(state => {
+      const filter = state.filter.toLowerCase();
+      const unsortedAnecdotes = state.anecdotes;
+      console.log('filter, unsortedAnecdotes', filter, unsortedAnecdotes)
+
+      // Filter anecdotes based on state.filter
+      const filteredAnecdotes = unsortedAnecdotes.filter(anecdote => anecdote.content.toLowerCase().includes(filter));
+
+      // Sort the filtered anecdotes by the number of votes in descending order
+      const sortedAnecdotes = filteredAnecdotes.slice().sort((a, b) => b.votes - a.votes);
+
+      return sortedAnecdotes;
+    });
+  
     const dispatch = useDispatch()
     const vote = (id) => {
         console.log('vote', id)
---
+++ part6/redux-anecdotes/src/reducers/filterReducer.js
@@ -0,0 +1,20 @@
+const filterReducer = (state = '', action) => {
+    console.log('state now: ', state)
+    console.log('action', action)
+    switch (action.type) {
+        case 'SET_FILTER':
+            return action.payload
+        default:
+            return state
+    }
+}
+
+export const filterChange = (filter) => {
+    console.log('filter',filter)
+    return {
+      type: 'SET_FILTER',
+      payload: filter
+    }
+}
+
+export default filterReducer
